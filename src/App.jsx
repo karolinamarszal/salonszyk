2e543e77636ec4968acd59a608cedfd47827b9d6
Karolina Marszal <karomarszal@gmail.com>
2023-02-08 19:43:17 +0100

    Added AboutUs comopnent

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Hero from './components/Hero'
+import AboutUs from './components/AboutUs'
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Header />
       <Hero />
+      <AboutUs />
     </div>
   );
 }
