f477e1b8382a28962c2aafef0047410b660b63c5
Karolina Marszal <karomarszal@gmail.com>
2023-02-09 18:31:38 +0100

    Added smaller logo for mobile display

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,12 @@
 import baner from '.././assets/baner.png'
+import logoxs from '.././assets/logoxs.png'
 
 function Hero() {
   return (
     <section>
-      <div>
+      <div className="container padding-block-600">
         <img src={baner} className="hero-img" alt='baner'></img>
+        <img src={logoxs} className="hero-img__sm" alt='logoxs'></img>
       </div>
     </section>
   )
