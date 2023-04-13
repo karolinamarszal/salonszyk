60abc534b9a493ad19825441920ae9eff2b7b5c9
Karolina Marszal <karomarszal@gmail.com>
2023-04-13 13:54:45 +0200

    Changed address

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
           <div className="even-columns">
             <div className="google-map">
               <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1572.157426164746!2d22.242733598078352!3d50.1529576541675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473ce90130075821%3A0xfef56553d0e7582f!2sRakszawa%20348%2C%2037-111%20Rakszawa!5e0!3m2!1spl!2spl!4v1675974518269!5m2!1spl!2spl" 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.431456639041!2d22.235131513851144!3d50.064011078864986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473cedd6ae1811c5%3A0x690189f0ff0a6a46!2sKwiatowa%2023%2C%2037-100%20%C5%81a%C5%84cut!5e0!3m2!1spl!2spl!4v1681387152520!5m2!1spl!2spl" 
                 width="600" 
                 height="450"
                 style={{ border:"0" }}
